83560e793c4cb1637909e54aa978487eb51c7f5a
Hassassistant <129045165+Hassassistant@users.noreply.github.com>
2023-06-06 12:44:04 +0100

    Add files via upload

---
+++ custom_components/openassist/__init__.py
@@ -0,0 +1,221 @@
+import logging
+import json
+import openai
+import requests
+import yaml
+import time
+import asyncio
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.const import EVENT_STATE_CHANGED
+
+
+DOMAIN = "openassist"
+EVENT_OPENASSIST_UPDATE = f"{DOMAIN}_update"
+
+_LOGGER = logging.getLogger(__name__)
+
+MODEL = "text-embedding-ada-002"
+
+index_name = "entities"
+
+with open('/config/.storage/core.entity_registry', 'r') as f:
+    entities = json.load(f)["data"]["entities"]
+
+
+
+def create_embedding(input, model):
+    _LOGGER.debug("Creating embedding")
+    return openai.Embedding.create(input=input, engine=model)['data'][0]['embedding']
+
+def post_request(url, headers, json_payload):
+    _LOGGER.debug("Sending POST request")
+    response = requests.post(url, headers=headers, json=json_payload)
+    return response.json()
+
+
+def get_request_pinecone(url, headers):
+    _LOGGER.debug("Sending GET request")
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+
+def get_request_pinecone_host(url, headers):
+    _LOGGER.debug("Sending GET request")
+    response = requests.get(url, headers=headers)
+    parsed_response = response.json()
+    return parsed_response, parsed_response.get('status', {}).get('host')
+
+
+
+
+def post_request_pinecone(url, headers, json_payload):
+    _LOGGER.debug(f"Sending POST request to URL: {url}")
+    _LOGGER.debug(f"POST data: {json.dumps(json_payload, indent=2)}")  # pretty print the JSON data
+    response = requests.post(url, headers=headers, json=json_payload)
+    if response.content:  # only attempt to parse if there's a response
+        return response.json()
+    else:
+        return None
+
+
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the OpenAI Assistant component."""
+    _LOGGER.debug("Setting up OpenAssist")
+
+    conf = config[DOMAIN] 
+    openai.api_key = conf['openai_key']
+    pinecone_env = conf['pinecone_env']
+    headers = {
+        'Api-Key': conf['pinecone_key'],
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+
+    async def state_change_handler(event):
+        """Handle an OpenAssist state change."""
+        entity_id = event.data.get("entity_id")
+        if entity_id == "input_text.openassist_prompt":
+            _LOGGER.debug("Handling state change event for openassist_prompt")
+            new_state = event.data.get("new_state")
+            if new_state is not None:
+                _LOGGER.debug("Generating embeddings for new state")
+                xq = await hass.async_add_executor_job(create_embedding, new_state.state, MODEL)
+                _LOGGER.debug("Embeddings generated, preparing payload for Pinecone")
+
+                # Fetch Pinecone host information
+                response = await hass.async_add_executor_job(
+                    get_request_pinecone,
+                    f'https://controller.{pinecone_env}.pinecone.io/databases/entities',
+                    headers
+                )
+                pinecone_host = response.get('status', {}).get('host')
+                if not pinecone_host:
+                    _LOGGER.error("Unable to fetch Pinecone host information.")
+                    return
+
+                url = f"https://{pinecone_host}/query"
+
+                payload = {
+                    "vector": list(xq),
+                    "includeMetadata": True,
+                    "topK": 1
+                }
+                _LOGGER.debug("Payload prepared, sending POST request to Pinecone")
+                response_json = await hass.async_add_executor_job(post_request, url, headers, payload)
+                _LOGGER.debug("POST request to Pinecone complete, processing response")
+                match = response_json['matches'][0]
+                _LOGGER.debug(f"Pinecone match: {match['score']:.2f}: {match['metadata']}")
+                _LOGGER.debug("Firing OpenAssist update event")
+                hass.bus.async_fire(EVENT_OPENASSIST_UPDATE, {"new_state": new_state.state, "metadata": match['metadata']})
+                _LOGGER.debug("OpenAssist update event fired")
+
+
+    async def state_change_handler_pinecone(event):
+        """Handle an OpenAssist state change."""
+        entity_id = event.data.get("entity_id")
+
+        if entity_id == "input_text.pinecone_index":
+            environment = event.data.get("new_state")
+            if environment is not None:
+                environment_str = environment.state
+
+                response = await hass.async_add_executor_job(
+                    get_request_pinecone,
+                    f'https://controller.{environment_str}.pinecone.io/databases',
+                    headers  # add headers here
+                )
+
+                existing_indexes = response
+                hass.states.async_set("sensor.openassist_response", "Building index", {"message": "Please wait while the Pinecone index gets built"})
+                
+                if index_name not in existing_indexes:
+                    # Create Pinecone index if it doesn't exist
+                    index_payload = {
+                        "name": index_name,
+                        "dimension": 1536,
+                        "metric": "cosine",
+                        "pods": 1,
+                        "replicas": 1,
+                        "pod_type": "p1.x1"
+                    }
+                    try:
+                        response = await hass.async_add_executor_job(post_request_pinecone, f'https://controller.{environment_str}.pinecone.io/databases', headers, index_payload)
+                        if response and 'status_code' in response:
+                            if response['status_code'] == 201:  # HTTP Status Code for 'Created'
+                                _LOGGER.debug(f"Index '{index_name}' has been created successfully.")
+                            else:
+                                _LOGGER.debug(f"Failed to create index. HTTP status code: {response['status_code']}. Response: {response['text']}")
+                                exit()  # Exit if we couldn't create the index
+                    except JSONDecodeError as e:
+                        _LOGGER.error(f"Error decoding JSON response: {e}")
+
+                # Wait until the index is ready
+                while True:
+                    response, host = await hass.async_add_executor_job(
+                        get_request_pinecone_host,
+                        f'https://controller.{environment_str}.pinecone.io/databases/{index_name}',
+                        headers
+                    )
+                    status = response.get('status', {}).get('state')
+                    if status == 'Ready':
+                        _LOGGER.debug("Index is ready.")
+                        _LOGGER.debug("Waiting an additional 3 minutes before beginning upsert operations...")
+                        hass.states.async_set("sensor.openassist_response", "Ready", {"message": "Index created! Data upload will begin in 3 minutes."})
+                        await asyncio.sleep(180)
+                        break
+
+                    else:
+                        _LOGGER.debug("Index is not ready yet, waiting for 5 seconds...")
+                        await asyncio.sleep(5)  # use asyncio.sleep instead of time.sleep
+
+                # Pinecone service url (updated to include the host)
+                url = f"https://{host}"
+
+                # Populate the index
+                hass.states.async_set("sensor.openassist_response", "Upserting data", {"message": "Upserting data. You will be notified once complete."})
+                for entity in entities:
+                    # Create a string representation of the entity
+                    entity_str = json.dumps(entity)
+                    # Create the embedding
+                    res = await hass.async_add_executor_job(create_embedding, entity_str, MODEL)
+                    _LOGGER.debug(f"res: {res}")
+                    embed = res
+
+
+                    # Create a new dictionary with only the fields we want
+                    metadata = {field: str(entity[field]) for field in ["device_id", "entity_id", "original_name", "platform", "unique_id", "unit_of_measurement"] if field in entity}
+
+                    # Define the payload to post
+                    payload = {
+                        "vectors": [
+                            {
+                                "id": entity["entity_id"],
+                                "values": list(embed),
+                                "namespace": "entities",
+                                "metadata": metadata  # not serializing the metadata
+                            }
+                        ]
+                    }
+
+                    # Make the POST request to Pinecone service
+                    response = await hass.async_add_executor_job(post_request_pinecone, f"{url}/vectors/upsert", headers, payload)
+                    if response and 'status_code' in response:
+                        if response['status_code'] == 200:  # HTTP Status Code for 'OK'
+                            _LOGGER.debug(f"Upsert response: {response['text']}")
+                        else:
+                            _LOGGER.debug(f"Failed to upsert. HTTP status code: {response['status_code']}. Response: {response['text']}")
+                hass.states.async_set("sensor.openassist_response", "Ready", {"message": "Your Pinecone index is ready to use! Enjoy."})
+
+
+
+
+
+
+    hass.bus.async_listen(EVENT_STATE_CHANGED, state_change_handler)
+    hass.bus.async_listen(EVENT_STATE_CHANGED, state_change_handler_pinecone)
+    return True
---
+++ custom_components/openassist/sensor.py
@@ -0,0 +1,205 @@
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.debug("OpenAssist component is being loaded")
+
+import aiohttp
+import voluptuous as vol
+import json
+import yaml
+
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
+import homeassistant.helpers.config_validation as cv
+
+from . import DOMAIN, EVENT_OPENASSIST_UPDATE
+
+DEFAULT_NAME = "OpenAssist Response"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required('mindsdb_model'): cv.string,
+    vol.Required('mindsdb_cookie'): cv.string,
+    vol.Optional('notify_device'): cv.string,
+    vol.Optional('your_name'): cv.string,
+})
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the OpenAssist sensor."""
+    _LOGGER.debug("Setting up OpenAssistSensor")
+
+    name = config.get(CONF_NAME, DEFAULT_NAME)
+    mindsdb_model = config['mindsdb_model']
+    mindsdb_cookie = config['mindsdb_cookie']
+    notify_device = config.get('notify_device', 'alexa_media_kitchen_echo')
+    your_name = config.get('your_name', '')
+    
+    add_entities([OpenAssistSensor(name, mindsdb_model, mindsdb_cookie, notify_device, your_name)])
+    _LOGGER.debug("OpenAssistSensor added to entities")
+
+
+
+
+class OpenAssistSensor(Entity):
+    """Representation of an OpenAssist Sensor."""
+
+    async def ask_mindsdb(self, prompt):
+        _LOGGER.debug("Asking MindsDB")
+        url = f"https://cloud.mindsdb.com/api/projects/mindsdb/models/{self._mindsdb_model}/predict"
+        cookies = {"session": self._mindsdb_cookie}
+        data = {"data": [{"text": prompt}]}
+        headers = {"Content-Type": "application/json"}
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json=data, cookies=cookies, headers=headers) as resp:
+                response_json = await resp.json()
+        try:
+            response_message = response_json[0]["response"]
+        except KeyError:
+            response_message = None
+        return response_message
+
+
+
+    def __init__(self, name, mindsdb_model, mindsdb_cookie, notify_device, your_name):
+        """Initialize the sensor."""
+        self._state = None
+        self._name = name
+        self._response = None  
+        self._message = None  
+        self._mindsdb_model = mindsdb_model
+        self._mindsdb_cookie = mindsdb_cookie
+        self._notify_device = notify_device
+        self._your_name = your_name
+        _LOGGER.info("OpenAssistSensor initialized")
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        return {"response": self._response, "message": self._message} 
+
+    async def async_added_to_hass(self):
+        """Run when entity about to be added to hass."""
+        _LOGGER.info("OpenAssistSensor added to hass, connecting to OpenAssist update event")
+        self.hass.bus.async_listen(EVENT_OPENASSIST_UPDATE, self._async_handle_update)
+        _LOGGER.info("OpenAssistSensor connected to OpenAssist update event")
+
+
+    async def _async_handle_update(self, event):
+        """Handle the OpenAssist update event."""
+        _LOGGER.info("OpenAssistSensor received update event")
+        new_state = event.data.get("new_state", "")
+        if not new_state:
+            _LOGGER.info("Input text is empty. Skipping...")
+            return
+        metadata = event.data.get("metadata", "")
+        _LOGGER.info(f"Event data: new_state={new_state}, metadata={metadata}")
+
+        
+        prompt = (
+            "If the query is a generic question, respond with:\n\n"
+            "{\n"
+            f"  \"message\": \"Generate a humane response (reference current user {self._your_name})\"\n"
+            "}\n\n"
+
+            "If the query is for the current state of an entity, For example, question: is the (device) on?\n\n"
+            "{\n"
+            "  \"message\": \"The current state of the (original_name) is {{ states() }}\"\n"
+            "}\n\n"
+            
+            "device attributes is the following format, {{ state_attr('domain.entity', 'attribute') }}\n\n"
+
+            "If the query requires a call service, please format your response as a valid JSON object in the following way:"
+            "\n\n{\n"
+            "  \"domain\": \"The domain of the service\",\n"
+            "  \"service\": \"The service to be called\",\n"
+            "  \"entity_id\": \"The entity id to be affected\",\n"
+            "  \"data\": {\n"
+            "    \"key1\": \"value1\",\n"
+            "    \"key2\": \"value2\",\n"
+            "    ... and so on ...\n"
+            "  },\n"
+            "  \"message\": \"Confirmation message\"\n"
+            "}\n\n"
+            "For example, if the question asks to play 'Planet Rock' on the Amazon Echo device named 'Loft Echo', "
+            "you should respond with:\n\n"
+            "{\n"
+            "  \"domain\": \"media_player\",\n"
+            "  \"service\": \"play_media\",\n"
+            "  \"entity_id\": \"media_player.loft_echo\",\n"
+            "  \"data\": {\n"
+            "    \"media_content_id\": \"play Planet Rock\",\n"
+            "    \"media_content_type\": \"custom\"\n"
+            "  },\n"
+            "  \"message\": \"(Generate a message confirming completion of task)\"\n"
+            "}\n\n"
+            "Remember, all the keys in the dictionary ('domain', 'service', 'entity_id', 'data', 'message') are required. "
+            "The 'data' key should always have a dictionary as its value, and this dictionary can contain any number of keys "
+            "and values, depending on what the service requires.\n"
+            f"Question: {new_state}\n\nData: {json.dumps(metadata, indent=4)}\n"
+        )
+
+
+        _LOGGER.info(f"Prepared prompt for GPT-4: {prompt}")
+
+        
+
+        _LOGGER.info("Getting GPT-4 response")
+        response = await self.ask_mindsdb(prompt)
+        self._response = response  
+        _LOGGER.info(f'GPT Response: \n{response}')
+        self._state = 'Response received'  
+        _LOGGER.info("Executing service")
+        await self.execute_service(self.hass, response)
+        _LOGGER.info("Sending notification")
+        message = json.loads(response).get('message') if response else None
+        if message:  
+            message = self.hass.helpers.template.Template(message, self.hass).async_render()
+        self._message = message
+        await self.send_notification(message)
+        _LOGGER.info("Updating Home Assistant state")
+        self.async_schedule_update_ha_state()
+        _LOGGER.info("Home Assistant state updated")
+
+
+    async def execute_service(self, hass, response):
+        if not response:
+            _LOGGER.error("No response to execute")
+            return
+        try:
+            service_dict = json.loads(response)
+        except json.JSONDecodeError:
+            _LOGGER.error("Could not decode response as JSON")
+            return
+        domain = service_dict.get('domain')
+        service = service_dict.get('service')
+        entity_id = service_dict.get('entity_id')
+        data = service_dict.get('data', {})  
+        if not all([domain, service, entity_id]):
+            _LOGGER.error("Response missing required fields")
+            return
+        if 'entity_id' not in data:
+            data['entity_id'] = entity_id
+        await hass.services.async_call(domain, service, data)
+
+
+    async def send_notification(self, message):
+        if not message:
+            _LOGGER.error("No message to send")
+            return
+        await self.hass.services.async_call("notify", self._notify_device, {"message": message})
