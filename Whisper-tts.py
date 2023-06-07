07f2123388a9df4af10cd0c1d75a939d8f58f816
Hassassistant <129045165+Hassassistant@users.noreply.github.com>
2023-06-07 13:18:19 +0100

    Create Whisper-tts.py
    This py script allows you to change the state of the OpenAssist Input Text using your voice via OpenAI Whisper.
    
    Next job will be to implement this into the Custom Component.

---
+++ Whisper-tts.py
@@ -0,0 +1,81 @@
+##########################################################################################################################
+# Just a quick experiment using OpenAI TTS whisper to update the state of the OpenAssist Prompt Input Text Entity State. #
+##########################################################################################################################
+
+import pyaudio
+import openai
+import wave
+import requests
+
+######### UPDATE THESE ############
+
+HOME_ASSISTANT_URL = "https://"
+
+HA_LONG_LIVED_ACCESS_TOKEN = ""
+
+openai.api_key = ""
+
+###################################
+
+
+CHUNK = 1024
+WIDTH = 2
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(p.get_format_from_width(WIDTH)))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+# Transcribe audio file
+audio_file = open(WAVE_OUTPUT_FILENAME, "rb")
+transcript = openai.Audio.transcribe(model="whisper-1", file=audio_file, response_format="text")
+print(transcript)
+
+# Define the URL for the Home Assistant API call
+url = f"{HOME_ASSISTANT_URL}/api/states/input_text.openassist_prompt"
+
+# Define the headers for the API call
+headers = {
+    "Authorization": f"Bearer {HA_LONG_LIVED_ACCESS_TOKEN}",
+    "content-type": "application/json",
+}
+
+# Define the data for the API call
+data = {
+    "state": transcript
+}
+
+# Make the API call
+response = requests.post(url, headers=headers, json=data)
+
+# Print the response
+print(response.json())
